9fa93a54142509de3fc7da5cf0f01b1c18f328ae
Paul Leveau <plawn.yay@juniorisep.com>
2020-12-14 23:11:33 +0900

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md', 'r') as f :
 
 setup(
     name='petit_downloader',
-    version='0.1.6',
+    version='0.1.7',
     description='Download files in an easier way',
     packages=['petit_downloader'],
     url='https://github.com/Plawn/petit_downloader',
